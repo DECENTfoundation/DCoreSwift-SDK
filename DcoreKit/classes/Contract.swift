8de67743422f70d093d271e59dfb21b3c80f05d6
Michal Grman <mike@decent.ch>
2018-04-18 11:13:44 +0200

    Initial commit

---
+++ DcoreKit/classes/Contract.swift
@@ -0,0 +1,18 @@
+//
+//  Contract.swift
+//  DcoreKit
+//
+//  Created by Michal Grman on 18/04/2018.
+//  Copyright © 2018 Decent. All rights reserved.
+//
+
+import Foundation
+
+public protocol Contract {
+    
+}
+
+
+extension Contract {
+    
+}
---
+++ DcoreKit/classes/Dcore.swift
@@ -0,0 +1,13 @@
+//
+//  Dcore.swift
+//  DcoreKit
+//
+//  Created by Michal Grman on 18/04/2018.
+//  Copyright © 2018 Decent. All rights reserved.
+//
+
+import Foundation
+
+public class Dcore: Contract {
+
+}
