95ce2103db73ee4cc60754212dbd48a9420f24f7
Michal Grman <mike@decent.ch>
2018-12-30 19:02:43 +0100

    Cleanup.

--- Sources/DcoreKit/Utils/ChainError.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public enum ChainError: Error {
-    case notFound(String)
-    case runtime(Any)
-    case addressFormat(String)
-    case illegal(String)
-    case underlying(Error)
-}
