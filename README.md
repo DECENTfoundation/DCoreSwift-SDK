3b9b03068891ad28d7eabcd45a46af059ec2b015
Michal Grman <mike@decent.ch>
2019-01-14 19:00:29 +0100

    Updated xcode build settings for tests and lib to correctly link dependencies.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # DcoreKit
 
 A description of this package.
+
+Install
+brew install automake
+brew install libtool
--- Scripts/build_libraries.sh
+++ Scripts/build_libraries.sh
@@ -3,7 +3,7 @@ set -ex
 
 SCRIPT_DIR=`dirname "$0"`
 
-# (cd "$SCRIPT_DIR" && sh build_secp256k1.sh)
+(cd "$SCRIPT_DIR" && sh build_secp256k1.sh)
 (cd "$SCRIPT_DIR" && sh build_crypto.sh)
 
 exit 0
@@ -12,5 +12,5 @@ exit 0
 # XCode build phase
 #
 # if [ ! -d "$SRCROOT/Libraries/secp256k1/lib" ] || [ ! -d "$SRCROOT/Libraries/openssl/lib" ]; then
-#  env -i PATH=$PATH sh "$SRCROOT/setup/build_libraries.sh"
+#  env -i PATH=$PATH sh "$SRCROOT/Scripts/build_libraries.sh"
 # fi 
