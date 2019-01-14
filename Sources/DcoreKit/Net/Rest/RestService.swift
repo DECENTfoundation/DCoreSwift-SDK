67a681ffff86869df6b556ff1799eeef537c68aa
Michal Grman <mike@decent.ch>
2019-01-14 22:37:21 +0100

    Added workaround for sequence calls via url session where second immediate call failed in error -1005. Updated version

--- Sources/DcoreKit/Net/Rest/RestService.swift
+++ Sources/DcoreKit/Net/Rest/RestService.swift
@@ -6,6 +6,7 @@ final class RestService: CoreRequestConvertible {
 
     private let url: URL
     private let session: URLSession
+    private let scheduler = SerialDispatchQueueScheduler(qos: .default)
     
     init(_ url: URL, session: URLSession? = nil) {
         self.url = url
@@ -14,6 +15,8 @@ final class RestService: CoreRequestConvertible {
     
     func request<Output>(using req: BaseRequest<Output>) -> Single<Output> where Output: Codable {
         return session.rx.data(request: req.asRest(url))
+            .observeOn(scheduler)
+            .delay(0.01, scheduler: scheduler) // Added delay for some weird bug with url session sequenced calls 
             .map { res in
                 do { return try res.parse(response: req) } catch let error {
                     throw error.asChainException()
