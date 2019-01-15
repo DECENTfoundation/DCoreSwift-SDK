79884c4331d9d0e18745808a0ea90cc91aea8491
Michal Grman <mike@decent.ch>
2019-01-15 12:48:08 +0100

    Renamed api-service as api provider and make it internal.

--- Sources/DcoreKit/Api/ApiProvider.swift
+++ Sources/DcoreKit/Api/ApiProvider.swift
@@ -4,9 +4,9 @@ public protocol BaseApi {
     var api: DCore.Api { get }
 }
 
-public struct ApiProvider {
+struct ApiProvider {
     
-    public let api: DCore.Api
+    let api: DCore.Api
     
     init(using api: DCore.Api) {
         self.api = api
