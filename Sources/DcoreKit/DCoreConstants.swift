27e2fc076e3d51f4099a4ad2baacef3faf8c1e20
Michal Grman <mike@decent.ch>
2019-01-16 16:15:03 +0100

    Fixed dynamic global props decoding, added date formatter for dcore.

--- Sources/DcoreKit/DCoreConstants.swift
+++ Sources/DcoreKit/DCoreConstants.swift
@@ -7,6 +7,7 @@ extension DCore {
         
         public static var timeout: TimeInterval = 30 // seconds
         public static var expiration: Int = 30 // seconds
+        public static var datePattern: String = "yyyy-MM-dd'T'HH:mm:ss"
         public static var dct: ChainObject = "1.3.0".chain.chainObject!
     }
 }
--- Sources/DcoreKit/Net/Serialization/JsonContext.swift
+++ Sources/DcoreKit/Net/Serialization/JsonContext.swift
@@ -2,15 +2,19 @@ import Foundation
 import BigInt
 
 extension JSONDecoder {
-    public static func codingContext() -> JSONDecoder {
+    public static func codingContext(_ datePattern: String = DCore.Constant.datePattern) -> JSONDecoder {
         return JSONDecoder(context: [
             BigInt.CodingContext.key: BigInt.CodingContext.decimal
-        ])
+            ], datePattern: datePattern)
     }
     
-    convenience init(context: [CodingUserInfoKey: Any]) {
+    convenience init(context: [CodingUserInfoKey: Any], datePattern pattern: String) {
+        let format = DateFormatter()
+        format.dateFormat = pattern
+        
         self.init()
         self.userInfo = context
+        self.dateDecodingStrategy = .formatted(format)
     }
 }
 
