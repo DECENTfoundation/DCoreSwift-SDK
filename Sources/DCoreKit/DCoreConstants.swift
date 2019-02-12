28ee662283b7c667a5e296d71246094628f55ea9
Lukas Vyletel <lukas@decent.ch>
2019-02-12 10:49:50 +0100

    Change constants from var to let

--- Sources/DCoreKit/DCoreConstants.swift
+++ Sources/DCoreKit/DCoreConstants.swift
@@ -5,8 +5,8 @@ extension DCore {
     
     public enum Constant {
         
-        public static var timeout: TimeInterval = 30 // seconds
-        public static var expiration: Int = 30 // seconds
-        public static var dct: ChainObject = "1.3.0".dcore.chainObject!
+        public static let timeout: TimeInterval = 30 // seconds
+        public static let expiration: Int = 30 // seconds
+        public static let dct: ChainObject = "1.3.0".dcore.chainObject!
     }
 }
