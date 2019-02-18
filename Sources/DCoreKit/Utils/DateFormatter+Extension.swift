b780f4c7ca52b6b03da21965784f8f41eb751001
Lukas Vyletel <lukas@decent.ch>
2019-02-18 17:39:38 +0100

    Fix incorrect timezone used for parsing dates

--- Sources/DCoreKit/Utils/DateFormatter+Extension.swift
+++ Sources/DCoreKit/Utils/DateFormatter+Extension.swift
@@ -2,10 +2,11 @@ import Foundation
 
 extension DateFormatter {
     static var standard: DateFormatter {
-        
+
         let format = DateFormatter()
         format.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
-        
+        format.timeZone = TimeZone(abbreviation: "UTC")
+
         return format
     }
 }
