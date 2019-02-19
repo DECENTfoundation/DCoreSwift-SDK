34945af82c624539ce3303d642b9eae52a3fd95e
Lukas Vyletel <lukas@decent.ch>
2019-02-19 12:37:45 +0100

    Fix transfer observable stream not getting completed

--- Tests/DCoreKitTests/OperationApiTests.swift
+++ Tests/DCoreKitTests/OperationApiTests.swift
@@ -10,8 +10,8 @@ class OperationApiTests: XCTestCase {
     func testTransferOperation() {
         let pk = "5J1HnqK3gajNzDWj9Na6fo3gxtphv6MHLE5YLgRmQv8tC8e3rEd"
         let creds = try? Credentials("1.2.17".dcore.chainObject!, wif: pk)
-        
-        let confirm = try? wss.operation.transfer(creds!, to: "1.2.34", amount: AssetAmount(1000000), message: "Ahoj",
+
+        let confirm = try? wss.operation.transfer(creds!, to: "1.2.34", amount: AssetAmount(1), message: "Ahoj",
                                                   encrypted: false).debug().toBlocking().single()
         XCTAssertNotNil(confirm)
     }
