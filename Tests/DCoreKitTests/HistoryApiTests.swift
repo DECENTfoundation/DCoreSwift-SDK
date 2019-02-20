8ae6c22a08277bfadbdc962b6120db5e0cb7dd82
Michal Grman <mike@decent.ch>
2019-02-20 14:48:43 +0100

    Disable parsing of ops other then transfer, added helper for ops.

--- Tests/DCoreKitTests/HistoryApiTests.swift
+++ Tests/DCoreKitTests/HistoryApiTests.swift
@@ -7,18 +7,26 @@ import RxBlocking
 class HistoryApiTests: XCTestCase {
     
     private let rest = DCore.Sdk.create(forRest: "https://stagesocket.decentgo.com:8090/rpc")
+    private let wss = DCore.Sdk.create(forWss: "wss://stagesocket.decentgo.com:8090")
     
-    func testGetBalanceHistoryUsingRest() {
+    func testGetBalanceHistoryUsingWss() {
         let id = "1.2.17".dcore.chainObject!
     
-        let result = try? rest.history.getBalanceHistory(for: id, assets: [DCore.Constant.dct]).toBlocking().single()
+        let result = try? wss.history.getBalanceHistory(for: id).toBlocking().single()
         XCTAssertNotNil(result)
     }
     
- 
+    func testGetBalanceHistoryCheckTransferUsingRest() {
+        let id = "1.2.17".dcore.chainObject!
+        
+        let result = try? rest.history.getBalanceHistory(for: id, assets: [DCore.Constant.dct]).toBlocking().single()
+        XCTAssertNotNil(result)
+        XCTAssert(!result!.isEmpty)
+        XCTAssert(result!.first!.history.operation.is(type: TransferOperation.self))
+    }
     
-   
     static var allTests = [
-        ("testGetBalanceHistoryUsingRest", testGetBalanceHistoryUsingRest),
+        ("testGetBalanceHistoryUsingWss", testGetBalanceHistoryUsingWss),
+        ("testGetBalanceHistoryCheckTransferUsingRest", testGetBalanceHistoryCheckTransferUsingRest),
         ]
 }
