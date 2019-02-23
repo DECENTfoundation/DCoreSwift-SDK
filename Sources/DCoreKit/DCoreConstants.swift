b71a58afdc01d13701326f2c9b61e6aca3ffd529
Aleksey Potapov <bravesherman@gmail.com>
2019-02-23 18:33:20 +0100

    Constants file updated
    Added limit for assets

--- Sources/DCoreKit/DCoreConstants.swift
+++ Sources/DCoreKit/DCoreConstants.swift
@@ -9,5 +9,6 @@ extension DCore {
         public static let expiration: Int = 30 // seconds
         public static let dct: ChainObject = "1.3.0".dcore.chainObject!
         public static let dctQrPrefix = "decent"
+        public static let assetLimit: Int = 100 // max number of assets, that could be searched per one request
     }
 }
