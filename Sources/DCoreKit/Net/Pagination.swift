afff391cc9c87952f40cd56f57c3ef1dd976fce3
Michal Grman <mike@decent.ch>
2019-02-26 12:07:53 +0100

    Added limit update into pagination.

--- Sources/DCoreKit/Net/Pagination.swift
+++ Sources/DCoreKit/Net/Pagination.swift
@@ -9,6 +9,15 @@ public enum Pagination: Equatable {
     public static let ignore: Pagination = .page(bounds: .unset..<(.unset), offset: .unset, limit: 1000)
     public static let ignoreObject: Pagination = .pageObject(bounds: ObjectType.unset..<(ObjectType.unset), limit: 100)
     
+    public func update(limit: UInt64) -> Pagination {
+        switch self {
+        case .page(let bounds, let offset, _):
+            return .page(bounds: bounds, offset: offset, limit: limit)
+        case .pageObject(let bounds, _):
+            return .pageObject(bounds: bounds, limit: limit)
+        }
+    }
+    
     case
     pageObject(bounds: Range<ChainObject>, limit: UInt64),
     page(bounds: Range<UInt64>, offset: UInt64, limit: UInt64)
