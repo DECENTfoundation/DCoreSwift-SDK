b44ac4524ba195ebe8b5740c07ab2ed8f5764cc8
Lukas Vyletel <lukas@decent.ch>
2019-03-18 12:53:23 +0100

    Add method to convert String into QrTransfer

--- Tests/DCoreKitTests/QR/QrTransferTests.swift
+++ Tests/DCoreKitTests/QR/QrTransferTests.swift
@@ -23,6 +23,20 @@ class QrTransferTests: XCTestCase {
         )
     }
 
+    func testQrStringToQrTransfer() {
+        let input = "decent:abc?amount=1.2&asset=DCT&memo=some%20weird%20test%20text%20%3D%20and%20so%20on%20%25%20with%20special%20chars"
+
+        XCTAssertEqual(
+            input.asQrTransfer(),
+            QrTransfer(
+                accountName: "abc",
+                assetSymbol: "DCT",
+                amount: "1.2",
+                memo: "some weird test text = and so on % with special chars"
+            )
+        )
+    }
+
     static var allTests = [
         ("testInputToQrString", testInputToQrString),
     ]
