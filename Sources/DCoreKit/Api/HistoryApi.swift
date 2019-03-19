665afa8eba7411905fb29f67549f22bbd4387d08
Michal Grman <mike@decent.ch>
2019-03-19 18:21:24 +0100

    Added documentation.

--- Sources/DCoreKit/Api/HistoryApi.swift
+++ Sources/DCoreKit/Api/HistoryApi.swift
@@ -6,12 +6,62 @@ extension Pagination {
 }
 
 public protocol HistoryApi: BaseApi {
+    /**
+     Get balance operation change by account id and operation id.
+     
+     - Parameter id: Account id,
+     as `ChainObject` or `String` format.
+     - Parameter operationId: Operation id,
+     as `ChainObject` or `String` format.
+     
+     - Returns: `BalanceChange`.
+     */
     func get(byAccountId id: ChainObjectConvertible,
              operationId: ChainObjectConvertible) -> Single<BalanceChange>
+    
+    /**
+     Get account history of operations.
+     
+     - Parameter id: Account id,
+     as `ChainObject` or `String` format.
+     - Parameter pagination: `Pagination` object.
+     
+     - Returns: Array `[OperationHistory]` of operations,
+     performed by account, ordered from most recent to oldest.
+     */
     func getAll(byAccountId id: ChainObjectConvertible,
                 pagination: Pagination) -> Single<[OperationHistory]>
+    
+    /**
+     Get account history of operations.
+     
+     - Parameter id: Account id,
+     as `ChainObject` or `String` format.
+     - Parameter pagination: `Pagination` object.
+     
+     - Returns: Array `[OperationHistory]` of operations,
+     performed by account, ordered from most recent to oldest.
+     */
     func getAllRelative(byAccountId id: ChainObjectConvertible,
                         pagination: Pagination) -> Single<[OperationHistory]>
+    
+    /**
+     Returns the most recent balance operations on the named account.
+     This returns a list of operation history objects,
+     which describe activity on the account.
+     
+     - Parameter id: Account id,
+     as `ChainObject` or `String` format.
+     - Parameter assets: Assets ids,
+     default `[]` for all assets.
+     - Parameter recipientId: Partner account object id,
+     to filter transfers to specific account,
+     default `nil`, as `ChainObject` or `String` format.
+     as `ChainObject` or `String` format.
+     - Parameter pagination: `Pagination` object.
+     
+     - Returns: Array `[BalanceChange]` of balance changes.
+     */
     func findAll(byAccountId id: ChainObjectConvertible,
                  assets: [ChainObjectConvertible],
                  recipientId: ChainObjectConvertible?,
