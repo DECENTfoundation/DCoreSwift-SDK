05ec01ede9e98f082c0de87617eda1989ee21e90
Michal Grman <mike@decent.ch>
2019-03-21 11:01:27 +0100

    Added vote as data in options.

--- Sources/DCoreKit/Model/VoteId.swift
+++ Sources/DCoreKit/Model/VoteId.swift
@@ -1,3 +1,22 @@
 import Foundation
 
 public typealias VoteId = String
+
+// swiftlint:disable shorthand_operator
+
+extension VoteId {
+    func asVoteData() -> Data {
+        let results = matches(regex: "(\\d+)").compactMap { Int($0) }
+        guard let last = results.last, let first = results.first else { return Data.ofZero }
+        return Data() + Int((last << 8) | first)
+    }
+}
+
+extension Set where Element == VoteId {
+    func asVoteData() -> Data {
+        guard !isEmpty else { return Data.ofZero }
+        return reduce(into: UInt64(count).asUnsignedVarIntData(), { data, element in
+            data = data + element.asVoteData()
+        })
+    }
+}
