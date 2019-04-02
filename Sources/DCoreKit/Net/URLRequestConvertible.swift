b7c7d8d4d5ecf4ccae842898176d4314fb5bb5d5
Lukas Vyletel <lukas@decent.ch>
2019-04-02 13:21:58 +0200

    Make timeout configurable

--- Sources/DCoreKit/Net/URLRequestConvertible.swift
+++ Sources/DCoreKit/Net/URLRequestConvertible.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 protocol URLRequestConvertible {
-    func asURLRequest(_ url: URL) -> URLRequest
+    func asURLRequest(_ url: URL, timeout: TimeInterval) -> URLRequest
 }
 
 extension URLRequestConvertible {
-    func asURLRequest(_ url: URL) -> URLRequest {
-        return URLRequest(url: url, cachePolicy: .reloadIgnoringCacheData, timeoutInterval: DCore.Constant.timeout)
+    func asURLRequest(_ url: URL, timeout: TimeInterval) -> URLRequest {
+        return URLRequest(url: url, cachePolicy: .reloadIgnoringCacheData, timeoutInterval: timeout)
     }
 }
