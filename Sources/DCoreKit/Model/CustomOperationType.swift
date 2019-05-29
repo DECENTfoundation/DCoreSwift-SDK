a7b766cb862c18995cffcfb598d21907e41f6ee6
Lukas Vyletel <lukas@decent.ch>
2019-05-29 08:49:01 +0200

    Future-proof parsing CustomOperationType

--- Sources/DCoreKit/Model/CustomOperationType.swift
+++ Sources/DCoreKit/Model/CustomOperationType.swift
@@ -2,6 +2,12 @@ import Foundation
 
 public enum CustomOperationType: Int, Codable {
     case
+    unknown = -1,
     null,
     messaging
+
+    public init(from aDecoder: Decoder) throws {
+        let container = try aDecoder.singleValueContainer()
+        self = CustomOperationType(rawValue: try container.decode(Int.self)) ?? .unknown
+    }
 }
