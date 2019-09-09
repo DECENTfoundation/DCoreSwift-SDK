458e80689bd44b1149698d8bfb1456da76465c06
Lukas Vyletel <lukas@decent.ch>
2019-09-09 15:46:44 +0200

    Add extension method for parsing dates from DCore

--- Sources/DCoreKit/Utils/Date+DCoreExtensionProvider.swift
+++ Sources/DCoreKit/Utils/Date+DCoreExtensionProvider.swift
@@ -6,4 +6,8 @@ extension DCoreExtension where Base == Date {
     public var description: String {
         return DateFormatter.standard.string(from: self.base)
     }
+
+    public static func date(from date: String) -> Date? {
+        return DateFormatter.standard.date(from: date)
+    }
 }
